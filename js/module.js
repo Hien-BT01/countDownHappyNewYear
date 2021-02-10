d373c14f99cbf919bd748244187e1751cf833101
Hien-BT01 <hienbui25g@gmail.com>
2021-02-10 21:04:56 +0700

    Happy new year

--- js/module.js
+++ js/module.js
@@ -3,7 +3,10 @@ const svgContainer = document.querySelector("#svg");
 const svgContainer1 = document.querySelector("#svg1");
 const svgContainer2 = document.querySelector("#svg2")
 const newYear = document.querySelector(".newyear");
-const audio = document.querySelector("audio");
+let audioE = document.querySelector("audio");
+const playButton = document.querySelector(".play-music")
+const pauseButton = document.querySelector(".pause-music")
+console.log(playButton)
 function remainingTime() {
   const today = new Date().getTime();
   const timeleft = futureDay - today;
@@ -26,8 +29,8 @@ function remainingTime() {
     time.innerHTML = format(values[index]);
   });
   if (timeleft <= 0) {
-    audio.muted = false;
-    audio.play();
+    playButton.classList.add("appear");
+    pauseButton.classList.add("appear");
     times.forEach(time => {
         time.innerHTML = "00";
     })
@@ -41,12 +44,12 @@ function remainingTime() {
     setInterval(newAnimeItem.goToAndPlay(0, true), 2950);
     setInterval(fireworkAnimation.goToAndPlay(0,true),2500);
     setInterval(() => {
-      svgContainer.style.top = `${Math.floor(Math.random() * 10)}%`;
-      svgContainer.style.left = `${Math.floor(Math.random() * 30)}%`;
-      svgContainer1.style.top = `${Math.floor(Math.random() * 15)}%`;
-      svgContainer1.style.right = `${Math.floor(Math.random() * 30)}%`;
+      svgContainer.style.top = `${Math.floor(Math.random() * 20)}%`;
+      svgContainer.style.left = `${Math.floor(Math.random() * 50)}%`;
+      svgContainer1.style.top = `${Math.floor(Math.random() * 20)}%`;
+      svgContainer1.style.right = `${Math.floor(Math.random() * 50)}%`;
       svgContainer2.style.left = `${Math.floor(Math.random() * 80)}%`;
-      svgContainer2.style.top = `${Math.floor(Math.random() * 50)}%`;
+      svgContainer2.style.top = `${Math.floor(Math.random() * 2020)}%`;
     }, 2350);
   }
 }
@@ -73,8 +76,9 @@ const fireworkAnimation = bodymovin.loadAnimation({
   autoplay: false,
   path:"https://assets7.lottiefiles.com/datafiles/068H91NVUJLGN4p/data.json",
 })
-
-window.onload = function(){
-  audio.muted = true;
-  audio.play();
-}
+playButton.addEventListener("click",e => {
+  audioE.play();
+})
+pauseButton.addEventListener("click",e => {
+  audioE.pause();
+})
