aabaa93ef59f64ea412b7eac75d0cb664deb9810
Hien-BT01 <70765122+Hien-BT01@users.noreply.github.com>
2022-01-31 10:01:14 +0700

    Update variable.js

--- js/variable.js
+++ js/variable.js
@@ -1,3 +1,3 @@
 
-export const futureDay = new Date(2021,1,12,0,0,0).getTime();
+export const futureDay = new Date(2022,1,1,0,0,0).getTime();
 export const times = document.querySelectorAll(".countdown-container h2");
